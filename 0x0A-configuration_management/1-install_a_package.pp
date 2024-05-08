77a940e3168bd7ffac5b3255b4ce6815ab092eb0
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-05-08 10:53:45 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # Using Puppet, install flask from pip3.
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install-flask':
+  command => 'pip3 install flask==2.1.0',
+  require => Package['python3-pip'],
 }
