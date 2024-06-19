a0ace4b57de8454d741d1ef72b0fb45e8ec45546
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-06-19 11:37:10 +0100

    Create 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Module containing subreddit api tasks'''
+import requests
+
+def top_ten(subreddit):
+    '''
+    rhis queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+    If an invalid subreddit is given, the function should print None.
+    '''
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0"
+            }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data")
+        [print(item.get("data").get("title")) for item in results.get("children")]
+    else:
+        print("None")
